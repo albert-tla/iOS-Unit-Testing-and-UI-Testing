18ea2691c9c39a2454a2c240b8c6e6529ac404ef
Albert Pangestu <albert.pangestu@pegipegi.com>
2022-06-20 07:57:43 +0700

    Set setUpWithError and tearDownWithError

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -27,15 +27,22 @@
 /// THE SOFTWARE.
 
 import XCTest
+@testable import BullsEye
 
 class BullsEyeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+  
+  var sut: BullsEyeGame!
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    try super.setUpWithError()
+    sut = BullsEyeGame()
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    sut = nil
+    try super.tearDownWithError()
+  }
+  
 }
